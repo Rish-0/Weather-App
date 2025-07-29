f2bc10cb0baf9739756118d2ea7f0d48c4aac31f
Rish-0 <luciferhnerf@gmail.com>
2025-07-29 18:57:21 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,5 +1,5 @@
     const apiKey="";
-    const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+    const apiUrl="";
     const searchBox=document.querySelector(".cityname");
     const searchBtn=document.querySelector(".search");
     const weatherIcon=document.querySelector(".weather-icon");
